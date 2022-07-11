2ef621f43bf76eca07a1cc5915fd14b51a6563e2
Kevin <kev776522.mg08@nycu.edu.tw>
2022-07-11 17:02:06 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import SignMessage from './SignMessage';
+import VerifyMessage from './VerifyMessage';
+
+const App = () => {
+  return (
+    <div className="flex flex-wrap">
+      <div className="w-full lg:w-1/2">
+        <SignMessage />
+      </div>
+      <div className="w-full lg:w-1/2">
+        <VerifyMessage />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/SuccessMessage.js
@@ -0,0 +1,13 @@
+const SuccessMessage = ({ message }) => {
+  if (!message) return null;
+
+  return (
+    <div className="alert alert-success">
+      <div className="flex-1">
+        <label>{message}</label>
+      </div>
+    </div>
+  );
+};
+
+export default SuccessMessage;
